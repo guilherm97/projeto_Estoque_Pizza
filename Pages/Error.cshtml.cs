bfbab6a1ec6112efef353af09a2fc23a12663532
Guilherme Da silva Barbarino <74333587+guilherm97@users.noreply.github.com>
2023-02-28 17:05:52 -0300

    Update Error.cshtml.cs

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Nova_pasta__4_.Pages;
+namespace Code.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
