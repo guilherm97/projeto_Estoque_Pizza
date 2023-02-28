949b7016ac6bdb1762cf21cb2d11807e067c542e
Guilherme Da silva Barbarino <74333587+guilherm97@users.noreply.github.com>
2023-02-28 17:03:40 -0300

    Update Pizza.cs

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -4,13 +4,19 @@ using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Models;
 
-public class Pizza{
+
+
+public class Pizza
+{
     public int Id { get; set; }
+
     [Required]
     public string? Nome { get; set; }
     public TamanhoDePizzas Tamanho { get; set; }
     public bool Borda { get; set; }
+
     [Range(0.01, 9999.99)]
     public decimal Preco { get; set; }
 }
+
 public enum TamanhoDePizzas { pequena, media, grande }
